2ca13f149ce653384ebf6399f8a0ecb1ce086e94
Tom Bricaud <tom.bricaud1@orange.fr>
2021-10-27 19:34:04 +0200

    Factories: added random method

--- model/factories.py
+++ model/factories.py
@@ -1,4 +1,6 @@
 import json
+import random
+from typing import Dict, Tuple
 
 
 class Factory:
@@ -7,13 +9,25 @@ class Factory:
         self.target_class = target_class
 
     def from_json(self, src: str):
-        obj = json.loads(src)
+        obj: Dict = json.loads(src)
         return self.target_class(**obj)
 
     def from_name(self, category: str, name: str):
         with open(self.json_file) as f:
-            root = json.load(f)
+            root: Dict = json.load(f)
 
-        obj = root[category][name]
+        obj: Dict = root[category][name]
+
+        return self.target_class(name, **obj)
+
+    def random(self, category: str = None):
+        with open(self.json_file) as f:
+            root: Dict = json.load(f)
+
+        if category is None:
+            candidates: Dict = random.choice(list(root.values()))
+
+        tuple: Tuple[str, Dict] = random.choice(list(candidates.items()))
+        name, obj = tuple
 
         return self.target_class(name, **obj)
