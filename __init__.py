36026bfae43a050492dd59c77c698f772d87f8e6
Tom Bricaud <tom.bricaud1@orange.fr>
2021-10-29 18:11:45 +0200

    Consumables factory in init

--- __init__.py
+++ __init__.py
@@ -1,8 +1,16 @@
+from factories.consumables_factories import (
+    BombFactory, ConsumableFactory, PotionFactory, ScrollFactory, SummonningStoneFactory
+)
 from factories.factory import GenericFactory
+from model.consumables import Consumable
 from model.equipment import Equipment, Weapon
 from model.npc import NPC
 
 npc_factory = GenericFactory(NPC.RESOURCE_FILE, NPC)
 weapon_factory = GenericFactory(Weapon.RESOURCE_FILE, Weapon)
 equipment_factory = GenericFactory(Equipment.RESOURCE_FILE, Equipment)
-
+consumable_factory = ConsumableFactory(Consumable.RESOURCE_FILE, Consumable)
+potion_factory = PotionFactory()
+bomb_factory = BombFactory()
+scroll_factory = ScrollFactory()
+summonning_stone_factory = SummonningStoneFactory(npc_factory)
